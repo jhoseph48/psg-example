b28d50dfbd48ab974d137803c8aa944b4305f5e4
Jhoseph Orellana <jhosephorellana48@gmail.com>
2025-05-25 21:35:51 -0400

    sesion 05

--- session05.py
+++ session05.py
@@ -1,2 +1,53 @@
 #ejemplo de valor
 print(type(1))
+
+variable = 100
+print(variable)
+print(type(variable))
+
+#valor 10 en base decimal
+print("base decimal")
+print(10)
+#valor 10 en binario
+print("base binaria")
+print(0b1010)
+#valor 10 en octal
+print("base octal")
+print(0o12)
+#valor 10 en hexadecimal
+print("base hexadecimal")
+print(0xa)
+
+#valor 0.5 flotante
+print(0.5)
+print(type(0.5))
+
+#valor 2.0e-3 flotante
+variable_6 = 2.0e-3
+print(variable_6)
+print(type(variable_6))
+
+a = 10
+b = 3
+#suma
+print(a+b)
+#resta 
+print(a-b)
+#multiplicacion
+print(a*b)
+#division
+print(a/b)
+#modulo
+print(a%b)
+#division entera
+print(a//b)
+#potencia
+print(a**b)
+
+#ejercicio contador
+minutos = 300
+segundos = 3600
+minutos = minutos + (segundos/60)
+horas = minutos/60
+print("hay:", horas, "horas")
+
